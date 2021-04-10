47ec089608166c732ddfaf6ff31c92abcce3740a
Anukriti <anu1999kriti@gmail.com>
2021-04-10 09:16:47 +0530

    Deploy

--- gulp/tasks/gulpfile.js
+++ gulp/tasks/gulpfile.js
@@ -1,7 +1,7 @@
 gulp.task('serveprod', function() {
   connect.server({
-    root: [your_project_path],
-    port: process.env.PORT || 5000, // localhost:5000
+    root: [__dirname],
+    port: process.env.PORT || 4000, // localhost:5000
     livereload: false
   });
 });
